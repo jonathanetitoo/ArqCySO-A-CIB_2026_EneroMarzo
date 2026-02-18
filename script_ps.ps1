84044d9b4b9f968b70a3093751e22aae5fe41042
Jonathan Tito <jotitoon@uide.edu.ec>
2026-02-18 02:08:23 +0000

    wip: ps

--- script_ps.ps1
+++ script_ps.ps1
@@ -2,3 +2,34 @@ $c = "chao"
 Write-Host $c
 Write-Output $c
 Write-Host "Hola" -ForegroundColor Green -BackgroundColor Black
+
+[int]$x = 3
+if ($x -gt 5) 
+{
+    Write-Host "Mayor que 5"
+} else 
+{
+    Write-Host "5 o menos"
+}
+
+Write-Host "Inicia foreach"
+$lista = @(1, 2, 3)
+foreach ($item in $lista) {
+Write-Host $item
+}
+Write-Host "Finaliza el foreach"
+
+Write-Host "Inicia el while"
+$x = 0
+while ($x -lt 3) {
+Write-Host $x
+$x++
+}
+Write-Host "Finaliza el while"
+
+Write-Host "Inicia funcion Sumar"
+Function Sumar($a, $b) {
+return $a + $b
+}
+Write-Host $(Sumar 5 7)
+Write-Host "Finaliza funcion Sumar"
